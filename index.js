42cf10356413765d3b1d3cc55fb1d3d713070853
Jéssica Hickembick <j.hickembick@gmail.com>
2023-08-29 00:46:36 -0300

    ATUALIZAÇAO DO CODIGO DO INDEX

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-// Importar o módulo readline para interação com o terminal
 const readline = require('readline');
 
-// Criar uma interface para leitura e escrita no terminal
 const rl = readline.createInterface({
-    input: process.stdin,   // Usar a entrada padrão (teclado)
-    output: process.stdout  // Usar a saída padrão (terminal)
+    input: process.stdin,
+    output: process.stdout
 });
 
-// Função para solicitar propriedades de CSS ao usuário
-function promptProperty() {
-    const properties = [];  // Inicia o array para armazenar as propriedades
+const properties = [];  // Array para armazenar as propriedades
 
-    // Pergunta as propriedade de CSS
+function promptProperty() {
     rl.question('Digite uma propriedade de CSS (ou "SAIR" para finalizar): ', (property) => {
         try {
-            // Verifica se foidigitado o "SAIR"
             if (property.toUpperCase() === 'SAIR') {
-                console.log('\nPropriedades ordenadas de A-Z com sucesso');
-                properties.sort();  // Ordenar as propriedades de A-Z
-                properties.forEach(prop => console.log(prop));  // Imprimir as propriedade
-                rl.close();  // Fechar o readline
+                properties.sort();
+                console.log('\nPropriedades ordenadas de A-Z:');
+                properties.forEach(prop => console.log(prop));
+                rl.close();
             } else {
-                properties.push(property);  // Adicionar a propriedade no array
-                promptProperty();  // Chama a função novamente para a próxima propriedade
+                properties.push(property);
+                properties.sort();  // Ordenar as propriedades imediatamente
+                promptProperty();   // Chamar a função novamente para a próxima propriedade
             }
         } catch (error) {
             console.error('Ocorreu um erro na sua solicitação', error.message);
-            rl.close();  // Fechar o readline em caso de erro
+            rl.close();
         }
     });
 }
 
-// Iniciar a solicitação de propriedades
 promptProperty();
