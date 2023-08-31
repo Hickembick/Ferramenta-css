8231e66ea329affb89b07acb5913a75e75a3777b
Jéssica Hickembick <j.hickembick@gmail.com>
2023-08-31 16:26:08 -0300

    criaçao do README

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # Ferramenta-css 
+# Ferramenta de Ordenação de Propriedades CSS 🛠️
+
+Bem-vindo(a) ao projeto **Ferramenta de Ordenação de Propriedades CSS**!
+Este é um projeto desenvolvido em Node.js que oferece uma ferramenta simples e eficiente para auxiliar no trabalho com propriedades CSS.
+
+Este projeto foi criado como requisito do curso de Formação Web Dev Fullstack - Resilia.
+
+ O contexto para a utilização da ferramenta é: Uma empresa de desenvolvimento de software precisa ordenar  uma lista de propriedades de CSS (ex: background-color, font-size, text-align) de A-Z.
+
+Com essa ferramenta, é possível inserir uma lista de propriedades e obter elas ordenas alfabeticamente visualizando o resultado no terminal.
+
+## 🚀 Tecnologias
+
+Desenvolvido com as seguintes tecnologias:
+
+- [Node.js](https://nodejs.org/en/)
+- [readline-sync](https://www.npmjs.com/package/readline-sync)
+
+## 📍 Executando Localmente
+
+**Certifique-se de ter o Node.js instalado em seu sistema.**
+- [NodeJS - Download](https://nodejs.org/pt-br/download)
+
+**Clone o projeto:**
+
+- Com HTTPS:
+```bash
+git clone https://github.com/Hickembick/Ferramenta-css.git
+ 
+ - Com chave SSH:
+git clone https://github.com/Hickembick/Ferramenta-css.git
+
+Entre no diretório do projeto:
+cd Ferramenta-CSS
+
+Abra com VSCode (opcional):
+code .
+
+Instale as dependências:
+npm install
+
+Inicie o servidor:
+npm start OU npm run start
+
+Agora aproveite o projeto!
+📄 Documentação da API
+Ordenar Propriedades CSS em Ordem Alfabética
+bash
+npm start
+
+Parâmetro:
+propriedades -	array	Obrigatório (Lista de propriedades CSS a serem ordenadas.)
+SAIR -	string	(Obrigatório. Encerra a entrada.)
+Erros -	mensagem	Opcional (Mensagens de erro exibidas em cenários inesperados.)
+
+Entrada - Inserir uma lista de propriedades CSS:
+Digite uma propriedade CSS (ou "SAIR" para encerrar):
+
+Entrada - Inserir uma lista de propriedades CSS:
+Digite uma propriedade CSS (ou "SAIR" para encerrar):
+padding   
+[enter]
+
+Digite uma propriedade CSS (ou "SAIR" para encerrar):
+background-color
+[enter]
+
+font-family
+[enter]
+
+SAIR
+[enter]
+
+Saída - Propriedades ordenadas:
+background-color
+font-family
+padding
+
+Feito com 💜 por JessicaHickembick
+
+📖 Referências
+Documentação do Node.js (https://nodejs.org/en/docs)
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -9,7 +9,44 @@ const properties = [];  // Array para armazenar as propriedades
 
 function promptProperty() {
     rl.question('Digite uma propriedade de CSS (ou "SAIR" para finalizar): ', (property) => {
-        try {
+        try {// Importar o módulo readline para interação com o terminal
+            const readline = require('readline');
+            
+            // Criar uma interface para leitura e escrita no terminal
+            const rl = readline.createInterface({
+                input: process.stdin,   // Configura a entrada padrão para leitura (teclado)
+                output: process.stdout  // Configura a saída padrão para escrita (terminal)
+            });
+            
+            // Array para armazenar as propriedades inseridas pelo usuário
+            const properties = [];
+            
+            // Função para solicitar propriedades de CSS ao usuário
+            function promptProperty() {
+                // Pergunta ao usuário por uma propriedade de CSS
+                rl.question('Digite uma propriedade de CSS (ou "SAIR" para finalizar): ', (property) => {
+                    try {
+                        // Verifica se o usuário digitou "SAIR"
+                        if (property.toUpperCase() === 'SAIR') {
+                            properties.sort();  // Ordena as propriedades alfabeticamente
+                            console.log('\nPropriedades ordenadas de A-Z:');
+                            properties.forEach(prop => console.log(prop));
+                            rl.close();  // Fecha a interface readline
+                        } else {
+                            properties.push(property);  // Adiciona a propriedade no array
+                            properties.sort();  // Ordena as propriedades imediatamente
+                            promptProperty();   // Chama a função novamente para a próxima propriedade
+                        }
+                    } catch (error) {
+                        console.error('Ocorreu um erro na sua solicitação', error.message);
+                        rl.close();  // Fecha a interface readline em caso de erro
+                    }
+                });
+            }
+            
+            // Iniciar a solicitação de propriedades, chamando a função pela primeira vez
+            promptProperty();
+            
             if (property.toUpperCase() === 'SAIR') {
                 properties.sort();
                 console.log('\nPropriedades ordenadas de A-Z:');
