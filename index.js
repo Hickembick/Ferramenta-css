3250980bf8eb9b5f9f63bd2bc755254ed147c875
Jéssica Hickembick <j.hickembick@gmail.com>
2023-09-19 17:49:28 -0300

    ajustes no index

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Importa os módulos necessários
-const entrada = require('./model/entrada'); // Módulo para entrada de propriedades
-const processamento = require('./model/processamento'); // Módulo para processamento das propriedades
-const saida = require('./model/saida'); // Módulo para exibição das propriedades ordenadas
+const entrada = require('./model/entrada');
+const processamento = require('./model/processamento');
+const saida = require('./model/saida'); 
 
-// Obtém a interface readline (rl) e cria um array para armazenar as propriedades
-const rl = entrada.rl; // Obtém a interface readline do módulo de entrada
+
+// Obtém a interface readline (rl) e cria um array
+const rl = entrada.rl;
 const properties = [];
 
-// Inicia a função de entrada de propriedades, passando o array de propriedades e a interface readline como argumentos
+// Inicia a função de entrada de propriedades
 entrada.promptProperty(properties, rl);
 
 // Define um evento que é acionado quando a interface readline é fechada
