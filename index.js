59bd7f0325b0b6c6801fe6fa85dba7daa743fc1e
Jéssica Hickembick <j.hickembick@gmail.com>
2023-09-19 15:09:03 -0300

    criaçao dos modulos

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const entrada = require('./entrada');
-const processamento = require('./processamento');
-const saida = require('./saida');
+const readline = require('readline');
 
-const { promptProperty, rl } = entrada;
-const { processProperties } = processamento;
-const { displayProperties } = saida;
-
-// Iniciar a solicitação de propriedades
-promptProperty(() => {
-    rl.on('close', () => {
-        const properties = processProperties();
-        displayProperties(properties);
-    });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+
+const properties = [];  // Array para armazenar as propriedades
+
+function promptProperty() {
+    rl.question('Digite uma propriedade de CSS (ou "SAIR" para finalizar): ', (property) => {
+        try {
+            if (property.toUpperCase() === 'SAIR') {
+                rl.close();
+            } else {
+                properties.push(property);  // Adiciona a propriedade no array
+                promptProperty();   // Chama a função novamente para a próxima propriedade
+            }
+        } catch (error) {
+            console.error('Ocorreu um erro na sua solicitação', error.message);
+            rl.close();
+        }
+    });
+}
+
+rl.on('close', () => {
+    properties.sort();  // Ordena as propriedades alfabeticamente
+    console.log('\nPropriedades ordenadas de A-Z:');
+    properties.forEach(prop => console.log(prop));
+});
+
+promptProperty();
