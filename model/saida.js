a3fd96779a7ba5bef75ed1f99f855b3252df1837
Jéssica Hickembick <j.hickembick@gmail.com>
2023-09-19 17:35:33 -0300

    deleta pastas do model

--- model/saida.js
+++
@@ -1,8 +0,0 @@
-function displayProperties(properties) {
-    console.log('\nPropriedades ordenadas de A-Z:');
-    properties.forEach(prop => console.log(prop));
-}
-
-module.exports = {
-    displayProperties,
-};
