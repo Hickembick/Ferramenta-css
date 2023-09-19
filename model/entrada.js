d2be2ba0d3c8905bae4002561d7b8151f30171a6
Jéssica Hickembick <j.hickembick@gmail.com>
2023-09-19 15:41:19 -0300

    adiciona comentario na entrada

--- model/entrada.js
+++ model/entrada.js
@@ -1,13 +1,18 @@
+// Importa o módulo readline para interação com o terminal
 const readline = require('readline');
 
+// Cria uma interface de leitura/escrita para o terminal
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin,   // Configura a entrada padrão para leitura (teclado)
+    output: process.stdout  // Configura a saída padrão para escrita (terminal)
 });
 
-const properties = []; // Array para armazenar as propriedades
+// Array para armazenar as propriedades de CSS inseridas pelo usuário
+const properties = [];
 
+// Função que solicita ao usuário a entrada de propriedades de CSS
 function promptProperty(callback) {
+    // Pergunta ao usuário por uma propriedade de CSS
     rl.question('Digite uma propriedade de CSS (ou "SAIR" para finalizar): ', (property) => {
         if (property.toUpperCase() === 'SAIR') {
             rl.close();
@@ -18,6 +23,7 @@ function promptProperty(callback) {
     });
 }
 
+// Exporta as funções e a interface readline para uso em outros módulos
 module.exports = {
     promptProperty,
     rl,
